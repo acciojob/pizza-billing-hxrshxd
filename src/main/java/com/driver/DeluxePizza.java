9d48eaaa794ac591ef3a1a37d86f513ae4b751bf
Harshad Rathod <rharshad26@gmail.com>
2023-03-20 15:55:57 +0530

    checking for errors

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,9 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+//        super.setExtraCheese(true);
+//        super.setExtraToppings(true);
+        super.addExtraCheese();
+        super.addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,15 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean extraCheese = false;
+    private boolean extraToppings = false;
+    private boolean papperBag = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if (isVeg) this.price = 300;
+        else this.price = 400;
     }
 
     public int getPrice(){
@@ -17,18 +22,53 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (!extraCheese) {
+            this.price += 80;
+            extraCheese = true;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!extraToppings) {
+            if (isVeg) this.price += 70;
+            if (!isVeg) this.price += 120;
+            extraToppings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!papperBag) {
+            this.price += 20;
+            papperBag = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if (isVeg) this.bill = "Base Price Of The Pizza: 300\n";
+        if (!isVeg) this.bill = "Base Price Of The Pizza: 400\n";
+
+        if (extraCheese) this.bill += "Extra Cheese Added: 80\n";
+
+        if (extraToppings) {
+            if (isVeg) this.bill += "Extra Toppings Added: 70\n";
+            if (!isVeg) this.bill += "Extra Toppings Added: 120\n";
+        }
+
+        if (papperBag) this.bill += "Paperbag Added: 20\n";
+
+        this.bill += "Total Price: " + this.price + "\n";
         return this.bill;
+    }
+
+    public void setExtraCheese(boolean extraCheese) {
+        this.extraCheese = extraCheese;
+    }
+
+    public void setExtraToppings(boolean extraToppings) {
+        this.extraToppings = extraToppings;
     }
 }
